9203425fa7cdc57de9234b7efbd8c74dd75b3a7a
AyseIlg <119274748+AyseIlg@users.noreply.github.com>
2022-12-06 11:58:02 -0500

    added the search engine

--- src/index.js
+++ src/index.js
@@ -40,8 +40,17 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
-let apiKey = "54ta40282dof71f561bc5ad10a73608a";
-let city = "Paris";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "54ta40282dof71f561bc5ad10a73608a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
